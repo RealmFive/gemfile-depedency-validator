0b7bab8ac44a17f0354dfe090af8ccacad76d30d
antwali <98063586+antwali@users.noreply.github.com>
2022-07-20 10:22:06 -0500

    dependencies status json file output was added

--- gem-list.rb
+++ gem-list.rb
@@ -1,3 +1,9 @@
+#This gives out the list of dependencies that are not necessary and prints all of the in the 'dependency_status.json' file
+#by specifying if they are required or not. It firsts checks if there was any gems that were run in the past (in the json file) and
+#then continues from there.
+
+require 'json'
+
 class Dependencies
     class << self
         def list_of_dependencies()
@@ -13,32 +19,54 @@ class Dependencies
             `git reset --hard && bundle install`
         end
 
+        def load_statuses
+            if File.exists?('dependency_status.json')
+                json =File.read('dependency_status.json')
+                JSON.parse(json)
+            else
+                {}
+            end
+        end
+
+        def unload_statuses(statuses)
+            json= JSON.unparse(statuses) #generate json string from the hash
+            File.write('dependency_status.json', json) # hashes to file
+        end
+
         def test_method(dependencies)
             Dir.chdir("../periodic-pigeon")
             output =`git status --porcelain --untracked-files=no`
-
-            if output.empty? ==true
-                dependencies.select do |dependency|
-                    puts "removing #{dependency}"
-                    `bundle remove #{dependency}`
-                    puts "running tests"
-                    `rails test`
-                    success= ($?.exitstatus == 0)
-                    puts "reseting"
-                    `git reset --hard && bundle install`
-                    success
-                end
-            else
+            unless output.empty? 
                 puts "File was modified after previous commit"
-                puts output
+                return
             end
-            
+            statuses = load_statuses
+                
+            dependencies.each do |dependency|
+                if statuses.key?(dependency)
+                    puts "skipping #{dependency}"
+                    next
+                end
+
+                puts "removing #{dependency}"
+                `bundle remove #{dependency}`
+                `bundle clean --force`  #This command cleans up all unnecessary dependencies!
+                puts "running tests"
+                `rails test`
+                statuses[dependency]=($?.exitstatus != 0) #false means safe to go
+                unload_statuses(statuses)
+                puts "reseting"
+                `git reset --hard && bundle install`
+            end
+            statuses.select{|k, v| !v}.keys
         end
     end
 end
 
 dependencies =Dependencies.list_of_dependencies()
 unnecessary =Dependencies.test_method(dependencies)
+
+
 puts "These dependencies are not necessary"
 unnecessary.each {|u| puts "- #{u}"}
 
---
+++ list.rb
@@ -0,0 +1,52 @@
+#This was the initial program that shows out the list of dependencies that are not needed without puting them in another json file
+# 
+
+class Dependencies
+  class << self
+      def list_of_dependencies()
+          f=File.open('../periodic-pigeon/Gemfile')
+          list= []
+          f.each do |line|
+              if line.index("gem") == 0
+                  list << line.split[1].gsub("'", "").gsub(",","")
+              end
+          end
+          f.close
+          list
+      end
+
+      def reset(dependencies)
+      Dir.chdir("../periodic-pigeon")
+          `git reset --hard && bundle install`
+      end
+
+      def test_method(dependencies)
+          Dir.chdir("../periodic-pigeon")
+          output =`git status --porcelain --untracked-files=no`
+
+          if output.empty? ==true
+              dependencies.select do |dependency|
+                  puts "removing #{dependency}"
+                  `bundle remove #{dependency}`
+                  puts "running tests"
+                  `rails test`
+                  success= ($?.exitstatus == 0)
+                  puts "reseting"
+                  `git reset --hard && bundle install`
+                  success
+              end
+          else
+              puts "File was modified after previous commit"
+              puts output
+          end
+          
+      end
+  end
+end
+
+dependencies =Dependencies.list_of_dependencies()
+unnecessary =Dependencies.test_method(dependencies)
+puts "These dependencies are not necessary"
+unnecessary.each {|u| puts "- #{u}"}
+
+# Dependencies.reset(dependencies)
